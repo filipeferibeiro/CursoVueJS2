318edaf0b60c2a87ca6faec6bac835571a23d0e4
Filipe Fernandes <filipeferibeiro@outlook.com>
2022-05-12 16:39:39 -0300

    Seção 6

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="app">
+		{{ title }}
+		<button @click="title += '#'">Alterar</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => {
+		return {
+			title: "Teste Data usando Vuw JS!!",
+		};
+	},
+};
+</script>
+
+<style scoped>
+#app {
+	background-color: chocolate;
+	color: #fff;
+}
+</style>
