873d346aa339fe1ee487e8cffd6c1394e9dbc714
Filipe Fernandes <filipeferibeiro@outlook.com>
2022-05-16 21:51:57 -0300

    Seção 9

---
+++ componentes-avancados/citacoes/src/App.vue
@@ -0,0 +1,54 @@
+<template>
+	<div id="app">
+		<span>
+			<button @click="componente = 'Citacoes'">Citações</button>
+			<button @click="componente = 'Sobre'">Sobre</button>
+		</span>
+		<keep-alive>
+			<component :is="componente" />
+		</keep-alive>
+	</div>
+</template>
+
+<script>
+import Citacoes from "./components/Citacoes";
+import Sobre from "./components/Sobre";
+
+export default {
+	components: { Citacoes, Sobre },
+	data: () => ({
+		componente: "Sobre",
+	}),
+};
+</script>
+
+<style>
+#app {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	font-size: 1.6rem;
+}
+
+button {
+	outline: none;
+	padding: 5px 15px 10px;
+	margin: 10px 5px;
+	border-radius: 10px;
+	font-size: 2rem;
+	font-weight: 300;
+	color: #fff;
+	background-color: rgba(0, 0, 0, 0.5);
+	border: 1px solid rgba(255, 255, 255, 0.18);
+
+	cursor: pointer;
+
+	transition: all 0.2s;
+}
+
+button:hover {
+	opacity: 70%;
+}
+</style>
---
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="citacao">
+		<slot name="autor" />
+		<slot />
+		<div class="fonte">
+			<slot name="fonte" />
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	activated() {
+		console.log("activated");
+	},
+	deactivated() {
+		console.log("deactivated");
+	},
+	created() {
+		console.log("created");
+	},
+	destroyed() {
+		console.log("destroyed");
+	},
+};
+</script>
+
+<style scoped>
+.citacao {
+	border: 1px solid #ddd;
+	background-color: rgba(0, 0, 0, 0.1);
+	padding: 25px;
+	max-width: 450px;
+	border-radius: 10px;
+}
+.fonte {
+	float: right;
+}
+</style>
