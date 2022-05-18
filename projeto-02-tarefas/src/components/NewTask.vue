e350d21430ce67ed84050c2c650bd70d3ffe526f
Filipe Fernandes <filipeferibeiro@outlook.com>
2022-05-18 00:00:39 -0300

    Seção 10 - Projeto 02 - Tarefas

---
+++ projeto-02-tarefas/src/components/NewTask.vue
@@ -0,0 +1,51 @@
+<template>
+	<div class="new-task">
+		<input
+			type="text"
+			v-model="name"
+			@keydown.enter="add"
+			class="form-element"
+			placeholder="Nova tarefa?"
+		/>
+		<button class="form-element" @click="add">+</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		name: "",
+	}),
+	methods: {
+		add() {
+			this.$emit("taskAdded", { name: this.name });
+			this.name = "";
+		},
+	},
+};
+</script>
+
+<style>
+.new-task {
+	margin: 35px;
+}
+.form-element {
+	outline: none;
+	font-size: 2rem;
+	border: 1px solid #fff;
+	padding: 5px 10px 8px;
+	color: #fff;
+}
+input.form-element {
+	width: 500px;
+	background: #fff2;
+	border-top-left-radius: 10px;
+	border-bottom-left-radius: 10px;
+}
+button.form-element {
+	border-left: node;
+	background-color: #2196f3;
+	border-top-right-radius: 10px;
+	border-bottom-right-radius: 10px;
+}
+</style>
---
+++ projeto-02-tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="task-grid">
+		<template v-if="tasks.length">
+			<TaskItem
+				v-for="(task, i) in tasks"
+				:key="task.name"
+				:task="task"
+				@taskDeleted="$emit('taskDeleted', i)"
+				@taskStateChanged="$emit('taskStateChanged', i)"
+			/>
+		</template>
+		<p v-else class="no-task">Sua vida está em dia :)</p>
+	</div>
+</template>
+
+<script>
+import TaskItem from "./TaskItem.vue";
+export default {
+	props: {
+		tasks: {
+			type: Array,
+			required: true,
+		},
+	},
+	components: { TaskItem },
+};
+</script>
+
+<style>
+.task-grid {
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+}
+.task-grid .task {
+	margin: 10px;
+}
+.no-task {
+	color: #aaa;
+	font-size: 1.7rem;
+}
+</style>
