e90c60ab1cef365dab2723171c256974239a6c93
Filipe Fernandes <filipeferibeiro@outlook.com>
2022-05-19 22:12:39 -0300

    Seção 11

---
+++ formularios/formulario-desafio/src/App.vue
@@ -0,0 +1,142 @@
+<template>
+	<div id="app">
+		<h1>Formulário Desafio</h1>
+		<div class="conteudo">
+			<form class="painel" v-if="!enviado">
+				<div class="cabecalho">Formulário</div>
+				<!-- Exercicio 01 -->
+				<NomeCompleto v-model="usuario.nomeCompleto" />
+				<Rotulo nome="Email">
+					<input type="email" v-model="usuario.email" />
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<input type="password" v-model="usuario.senha" />
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados?">
+					<span class="mr-4">
+						<input type="checkbox" v-model="usuario.armazenar" />
+						<span>{{ usuario.armazenar ? "Sim" : "Não" }}</span>
+					</span>
+				</Rotulo>
+				<hr />
+				<button @click.prevent="enviar">Enviar</button>
+				<!-- Criar uma formulário de registro -->
+				<!-- Nome completo (Nome e Sobrenome) -->
+				<!-- Email -->
+				<!-- Senha -->
+				<!-- Armazenar Dados? (Sim/Não) -->
+
+				<!-- Exercicio 02 -->
+				<!-- Só mostrar o fomulário de não tiver sido submetido -->
+				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
+
+				<!-- Exercicio 03 -->
+				<!-- Crie um componente personalizado NomeCompleto -->
+				<!-- Esse componente deve receber Nome e Sobrenome -->
+			</form>
+			<div class="painel" v-else>
+				<div class="cabecalho">Resultado</div>
+				<Rotulo nome="Nome">
+					<span>{{ usuario.nomeCompleto.nome }}</span>
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<span>{{ usuario.nomeCompleto.sobrenome }}</span>
+				</Rotulo>
+				<Rotulo nome="Email">
+					<span>{{ usuario.email }}</span>
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<span>{{ usuario.senha }}</span>
+				</Rotulo>
+				<Rotulo nome="Armazenar Dados?">
+					<span>{{ usuario.armazenar ? "Sim" : "Não" }}</span>
+				</Rotulo>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Rotulo from "./components/Rotulo.vue";
+import NomeCompleto from "./components/NomeCompleto.vue";
+
+export default {
+	name: "app",
+	components: { Rotulo, NomeCompleto },
+	data: () => ({
+		usuario: {
+			nomeCompleto: {
+				nome: "",
+				sobrenome: "",
+			},
+			email: "",
+			senha: "",
+			armazenar: false,
+		},
+		enviado: false,
+	}),
+	methods: {
+		enviar() {
+			this.enviado = true;
+		},
+	},
+};
+</script>
+
+<style>
+body {
+	background-color: #ececec;
+}
+
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+
+	display: flex;
+	flex-direction: column;
+}
+
+.conteudo {
+	display: flex;
+}
+
+.painel {
+	flex: 1;
+	background: #fff;
+	margin: 0px 10px;
+	padding: 20px;
+	border: 1px solid #aaa;
+	border-radius: 5px;
+}
+
+.painel .cabecalho {
+	width: 100%;
+	background-color: #ddd;
+	padding: 10px 0px;
+	border-radius: 5px;
+	font-size: 1.4rem;
+}
+
+#app form button {
+	float: right;
+	margin: 10px 0px;
+	padding: 10px 20px;
+	font-size: 1.4rem;
+	border-radius: 5px;
+	color: #fff;
+	background-color: #2196f3;
+}
+
+#app h1 {
+	font-weight: 200;
+	margin: 20px;
+	padding: 0;
+}
+
+.mr-4 {
+	margin-right: 40px;
+}
+</style>
---
+++ formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<Rotulo nome="Nome">
+			<input type="text" :value="value.nome" @input="mudarNome" />
+		</Rotulo>
+		<Rotulo nome="Sobrenome">
+			<input
+				type="text"
+				:value="value.sobrenome"
+				@input="mudarSobrenome"
+			/>
+		</Rotulo>
+	</div>
+</template>
+
+<script>
+import Rotulo from "./Rotulo.vue";
+export default {
+	props: {
+		value: {
+			type: Object,
+			required: true,
+		},
+	},
+	components: {
+		Rotulo,
+	},
+	methods: {
+		mudarNome(event) {
+			this.$emit("input", { ...this.value, nome: event.target.value });
+		},
+		mudarSobrenome(event) {
+			this.$emit("input", {
+				...this.value,
+				sobrenome: event.target.value,
+			});
+		},
+	},
+};
+</script>
+
+<style></style>
