887c567451b9a26ac0381b07af928d2af8eb1240
Filipe Fernandes <filipeferibeiro@outlook.com>
2022-06-21 13:54:03 -0300

    Seção 22 - Backend Projeto Base de Conhecimento - Rota Categories

--- projeto-06-base-de-conhecimento/backend/config/routes.js
+++ projeto-06-base-de-conhecimento/backend/config/routes.js
@@ -4,9 +4,27 @@ module.exports = (app) => {
 		.get(app.api.user.get)
 		// POST
 		.post(app.api.user.save);
+
 	app.route("/users/:id")
 		// GET
 		.get(app.api.user.getById)
 		// PUT
 		.put(app.api.user.save);
+
+	app.route("/categories")
+		// GET
+		.get(app.api.category.get)
+		// POST
+		.post(app.api.category.save);
+
+	// Cuidado com ordem! Tem que vir antes de /categories/:id
+	app.route("/categories/tree").get(app.api.category.getTree);
+
+	app.route("/categories/:id")
+		// GET
+		.get(app.api.category.getById)
+		// PUT
+		.put(app.api.category.save)
+		// REMOVE
+		.delete(app.api.category.remove);
 };
